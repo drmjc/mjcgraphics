efdbb82dd56cac86b770aae1bfc354a175453312
Mark Cowley <m.cowley0@gmail.com>
2012-07-24 06:12:18 +0000

    merged most of the Rd's within colours.R

--- R/colours.R
+++ R/colours.R
@@ -1,85 +1,160 @@
-#' Extend \code{\link[grDevices]{colours}} to return the hex values of the named colours. see
-#' \code{\link{colours}}
-#' @return a vector of hex values such as \dQuote{#FF0EF0}
+#' @name mjc_colours
+#' @title colours, hex-colours and rgb-colours
+#' @description A collection of functions to convert colours from names (aka \code{colours}),
+#' hex values (aka \code{hex}) and RGB 3 character numeric's (aka \code{rgb}).
+#' This is \emph{old} code, and is crying out for at least some S3-class love. It was written
+#' to facilitate the interpolation of lots of colours, which requires named colours to be 
+#' numerically represented. This code allows \code{\link{colour.step}} to do its thing, though
+#' newer code in the \code{RColorBrewer} package may make much of this redundant.
+#' 
+#' @section Allowed colour Formats:
+#' We've named the default colours provided by grDevices the \code{col} format, for want of a
+#' better name.
+#' \itemize{
+#' \item{col:}{"White", "white", "Black", "green"}
+#' \item{hex:}{"#FF00FF", "#FF00FFAE"}
+#' \item{rgb:}{matrix(c(0,255,0), 1, 3)}
+#' }
+#' 
+#' @details 
+#' grDevices provides \code{\link[grDevices]{colours}}, and \code{\link[grDevices]{col2rgb}}.
+#' We wanted a hexadecimal representation of
+#' colours which included an alpha channel, as well as methods for checking colours, converting
+#' between colour types, and generating a spectrum of colours.
+#' 
+#' @section colour types:
+#' \code{colours}, \code{colours.hex}, \code{colours.rgb}: 
+#' return the hex, or rgb representations of the named colours provided by 
+#' \code{\link[grDevices]{colours}} from \code{grDevices}.
+#' 
+#' @section colour checks:
+#' \code{is.colour}, \code{is.hex.colour}, \code{is.rgb.colour}: 
+#' methods
+#' for checking whether the input is one of the 3 colour types, the hex colour type, or rgb
+#' colour type, respectively.
+#'
+#' @section colour conversion:
+#' \code{colour2rgb}, \code{colour2hex}: convert from \code{col} format\cr
+#' \code{hex2rgb}, \code{hex2colour}: convert from hex format\cr
+#' \code{rgb2hex}, \code{rgb2colour}: convert from rgb format\cr
+#' Note, \code{hex2colour}: there are 256^3 possible hex's but only \code{length(colours())}
+#' possible outputs, so the closest colour for each \code{hex} is determined.
+#' This function is the opposite of \code{grDevices:col2rgb}, and also is
+#' the opposite of \code{colour2rgb}\cr
+#' Note, \code{rgb2colour}: there are 256^3 possible x's but only length(cols.rgb())
+#' possible outputs, so the closest colour for each x is determined.
+#' This function is the opposite of grDevices:col2rgb, and also is
+#' the opposite of colour2rgb
+#' 
+#' @section \code{inverse.colour}:
+#' inverse a \code{col}-format colour & return the \code{hex}-format colour
+#' 
+#' @param col a vector of values in the \code{colour} format: eg \dQuote{white}
+#' @param rgb a vector of colours in the \code{rgb} format; ie a 3-column matrix
+#'  of [0,255] values, one colour per row.
+#' @param hex a vector of colours in the hex format: eg \dQuote{#FF00FF}
+#' @param cols.rgb the pool of colours with which to map \code{hex} to in rgb format.
+#'   Leave as default to get the normal "R colours". NB: If \code{cols.rgb}
+#'   doesn't have names, then the index into cols will be
+#'   returned. If \code{cols.rgb} has valid names, then they will be returned.
+#' 
+#' @return \code{colours}, \code{colours.hex}, \code{colours.rgb}: a vector of
+#' col-format colours, hex-format colours, or matrix of rgb-format colours, 1 row per colour.\cr
+#' \cr
+#' \code{is.colour}, \code{is.hex.colour}, \code{is.rgb.colour}: logical.\cr
+#' \cr
+#' \code{colour2rgb}, \code{colour2hex}, \code{hex2rgb}, \code{rgb2hex}, \code{hex2colour}, \code{rgb2colour}: a vector of colour formats, converted from one to another.\cr
+#' \cr
+#' \code{hex2colour}: a vector of the name of R colours; eg \code{c("white", "magenta",
+#'   "lightblue")} or a vector of indices into the \code{cols.rgb} vector, if
+#'    no valid names were found in the \code{cols.rgb} object.
+#' \code{rgb2colour}: a vector of the name of R colours;
+#' eg \code{c("white", "magenta", "lightblue")}, or a vector of indices into the 
+#' \code{cols.rgb} vector, if no valid names were found in the \code{cols.rgb} object.\cr
+#' \cr
+#' \code{inverse.colour}: a vector of colours\cr
+#' 
+#' 
+#' @rdname mjc-colours
 #' @author Mark Cowley, 29/3/07
+#' 
+#' @examples
+#' colours()[1:5]
+#' # [1] "white"         "aliceblue"     "antiquewhite"  "antiquewhite1"
+#' # [5] "antiquewhite2"
+#' colours.hex()[1:5]
+#' #     white     aliceblue  antiquewhite antiquewhite1 antiquewhite2 
+#' # "#FFFFFF"     "#F0F8FF"     "#FAEBD7"     "#FFEFDB"     "#EEDFCC" 
+#' colours.rgb()[1:5,]
+#' #               red green blue
+#' # white         255   255  255
+#' # aliceblue     240   248  255
+#' # antiquewhite  250   235  215
+#' # antiquewhite1 255   239  219
+#' # antiquewhite2 238   223  204
+#' 
+#' is.colour("white") # TRUE
+#' is.colour("#FFFFFF") # FALSE
+#' is.colour(c(255,255,255)) # FALSE
+#' 
+#' is.hex.colour("white") # FALSE
+#' is.hex.colour("#FFFFFF") # TRUE
+#' is.hex.colour(c(255,255,255)) # FALSE
+#' 
+#' is.rgb.colour("white") # FALSE
+#' is.rgb.colour("#FFFFFF") # FALSE
+#' is.rgb.colour(c(255,255,255)) # TRUE
+#' 
+#' colour2rgb("White")
+#' #       red green blue
+#' # White 255   255  255
+#' colour2hex("White")
+#' #     White 
+#' # "#FFFFFF" 
+#' hex2rgb( "#FF00E0" )
+#' #      red green blue
+#' # [1,] 255     0  224
+#' rgb2hex( matrix(c(255,128,255),1,3) )
+#' # [1] #FF80FF
+#' hex2colour( "#FF80FF" )
+#' # [1] "orchid1"
+#' rgb2colour(matrix(c(255,128,255), 1, 3))
+#' # [1] "orchid1"
+#' 
+#' inverse.colour("red")
+#' #       red 
+#' # "#00FFFF" 
+#' inverse.colour("orchid1")
+#' #   orchid1 
+#' # "#007C05"
+NULL
+
 #' @export
+#' @rdname mjc-colours
 colours.hex <- function() {
 	colour2hex( colours() )
 }
 
-#' Extend \code{\link[grDevices]{colours}} to return the rgb values of the named colours. see
-#' \code{\link{colours}}
-#' @return a vector of hex values such as ?
-#' @author Mark Cowley, 29/3/07
 #' @export
+#' @rdname mjc-colours
 colours.rgb <- function() {
 	colour2rgb( colours() )
 }
 
-
-
-########
-########
-## Check the three different colour formats.
-########
-########
-
-#' is "col" format
-#'
-#' Allowable colour Formats:\cr
-#' \itemize{
-#' \item{col}{examples: White, Black, green}
-#' \item{hex}{examples: "#FF00FF", "#FF00FFAE"}
-#' \item{rgb}{examples: c(0,255,0)}
-#' }
-#' @param col a vector of values
-#' @return logical: \code{TRUE} if col is in "col" format, \code{FALSE} otherwise.
-#' @author Mark Cowley, 2011-10-20
 #' @export
-#' @examples
-#' is.colour("white")
-#' is.colour("#FFFFFF")
-#' is.colour(c(255,255,255))
+#' @rdname mjc-colours
 is.colour <- function(col) {
 	is.character(col) && !any(grepl("^#", col))
 }
 
-#' is "hex" colour format
-#'
-#' Allowable colour Formats:\cr
-#' \itemize{
-#' \item{col}{examples: White, Black, green}
-#' \item{hex}{examples: "#FF00FF", "#FF00FFAE"}
-#' \item{rgb}{examples: c(0,255,0)}
-#' }
-#' @param col a vector of values
-#' @return logical: \code{TRUE} if col is in "col" format, \code{FALSE} otherwise.
-#' @author Mark Cowley, 2011-10-20
 #' @export
-#' @examples
-#' is.hex.colour("white")
-#' is.hex.colour("#FFFFFF")
-#' is.hex.colour(c(255,255,255))
+#' @rdname mjc-colours
 is.hex.colour <- function(hex) {
 	is.character(hex) && any(grepl("^#", hex))
 }
 
-#' is "rgb" colour format
-#'
-#' Allowable colour Formats:\cr
-#' \itemize{
-#' \item{col}{examples: White, Black, green}
-#' \item{hex}{examples: "#FF00FF", "#FF00FFAE"}
-#' \item{rgb}{examples: c(0,255,0)}
-#' }
-#' @param col a vector of values
-#' @return logical: \code{TRUE} if col is in "col" format, \code{FALSE} otherwise.
-#' @author Mark Cowley, 2011-10-20
 #' @export
-#' @examples
-#' is.rgb.colour("white")
-#' is.rgb.colour("#FFFFFF")
-#' is.rgb.colour(c(255,255,255))
+#' @rdname mjc-colours
 is.rgb.colour <- function(rgb) {
 	( is.matrix.like(rgb) && ncol(rgb) >= 3 ) ||
 	( is.numeric(rgb) && length(rgb) >= 3 )
@@ -98,154 +173,82 @@ is.rgb.colour <- function(rgb) {
 # rgb2colour
 #
 
-
-#' Convert "col" format to "rgb" format.
-#' 
-#' ie from "White" to c(255,255,255)
-#' 
-#' @param x a vector of colours in the colour format: \dQuote{white}
-#' @author Mark Cowley, 29/3/07
 #' @export
-#' @examples
-#' colour2rgb("White")
-#' # [1] c(255,255,255)
-colour2rgb <- function(x=NULL) {
-	stopifnot( is.colour(x) )
+#' @rdname mjc-colours
+colour2rgb <- function(col=NULL) {
+	stopifnot( is.colour(col) )
 
-	rgb <- t(grDevices::col2rgb(x))
-	rownames( rgb ) <- x
+	rgb <- t(grDevices::col2rgb(col))
+	rownames( rgb ) <- col
 	rgb
 }
 
-#' Convert "col" format to "rgb" format.
-#' 
-#' ie from "White" to "#FFFFFF"
-#' 
-#' @param x a vector of colours in the colour format: \dQuote{white}
-#' @author Mark Cowley, 29/3/07
 #' @export
-#' @examples
-#' colour2hex("White")
-#' # [1] "#FFFFFF"
-colour2hex <- function(x=NULL) {
-	stopifnot( is.colour(x) )
+#' @rdname mjc-colours
+colour2hex <- function(col=NULL) {
+	stopifnot( is.colour(col) )
 
-	rgb <- colour2rgb( x )
+	rgb <- colour2rgb( col )
 	hex <- rgb2hex( rgb )
-	names( hex ) <- x
+	names( hex ) <- col
 	hex
 }
 
-
-#' Convert "hex" format to "rgb" format.
-#' 
-#' ie from "#FF00EO" to c(256,0,224)
-#' 
-#' @param x a vector of colours in the hex format: \dQuote{#FF00FF}
-#' @author Mark Cowley, 29/3/07
 #' @export
-#' @examples
-#' hex2rgb( "#FF00EO" )
-#' # [1] c(256,0,224)
-hex2rgb <- function(x=NULL) {
-	stopifnot( is.hex.colour(x) )
+#' @rdname mjc-colours
+hex2rgb <- function(hex=NULL) {
+	stopifnot( is.hex.colour(hex) )
 
 	# trim the hash
-	x <- substring(x,2,7)
-	rgb <- cbind(red=as.numeric(paste0("0x",substring(x,1,2))),
-				 green=as.numeric(paste0("0x",substring(x,3,4))),
-				 blue=as.numeric(paste0("0x",substring(x,5,6))))
-	if( !is.null(names(x)) )
-		rownames(rgb) <- names(x)
+	hex <- substring(hex,2,7)
+	rgb <- cbind(red=as.numeric(paste0("0x",substring(hex,1,2))),
+				 green=as.numeric(paste0("0x",substring(hex,3,4))),
+				 blue=as.numeric(paste0("0x",substring(hex,5,6))))
+	if( !is.null(names(hex)) )
+		rownames(rgb) <- names(hex)
 	rgb
 }
 
-
-#' Convert "rgb" format to "hex" format.
-#' 
-#' ie from c(255,255,255) to "#FFFFFF"
-#' 
-#' @param x a vector of colours in the "rgb format" format; ie has three
-#'   columns of [0,255] values, one colour per row.
-#' @author Mark Cowley, 29/3/07
 #' @export
-#' @examples
-#' rgb2hex( matrix(c(255,255,255),1,3) )
-#' # [1] #FFFFFF
-rgb2hex <- function(x) {
-	stopifnot( is.rgb.colour(x) )
+#' @rdname mjc-colours
+rgb2hex <- function(rgb) {
+	stopifnot( is.rgb.colour(rgb) )
 
 	# convert an int in [0-255] into a character hex string 0 -> 00; 7 -> 07; 255 -> FF
-	.tohex2 <- function(x) {
-		x <- as.character.hexmode(x, upper.case=TRUE)
-		idx <- nchar(x)<2
+	.tohex2 <- function(rgb) {
+		rgb <- as.character.hexmode(rgb, upper.case=TRUE)
+		idx <- nchar(rgb)<2
 		if( any(idx) )
-			x[idx] <- paste("0", x[idx], sep="")
-		x
+			rgb[idx] <- paste("0", rgb[idx], sep="")
+		rgb
 	}
-	cols <- apply(x, 1, function(x) paste(c("#", .tohex2(x)), collapse=""))
+	cols <- apply(rgb, 1, function(rgb) paste(c("#", .tohex2(rgb)), collapse=""))
 
 	cols
 }
 
 
-#' Convert "hex" format to "col" format.
-#' 
-#' ie from "#FFFFFF" to "White"
-#' Note, there are 256^3 possible x's but only length(colours())
-#' possible outputs, so the closest colour for each x is determined.
-#' This function is the opposite of grDevices:col2rgb, and also is
-#' the opposite of colour2rgb
-#' 
-#' @param x a vector of colours in the hex format: \dQuote{#FF00FF}
-#' @param cols.rgb the pool of colours with which to map \code{x} to in rgb format.
-#'   Leave as default to get the normal "R colours". NB: If \code{cols.rgb}
-#'   doesn't have names, then the index into cols will be
-#'   returned. If \code{cols.rgb} has valid names, then they will be returned.
-#' @return a vector of the name of R colours; eg \code{c("white", "magenta",
-#'   "lightblue")} or a vector of indices into the \code{cols.rgb} vector, if
-#'    no valid names were found in the \code{cols.rgb} object.
-#' @author Mark Cowley, 29/3/07
 #' @export
-#' @examples
-#' hex2colour( "#FFFFFF" )
-#' # [1] "white"
-hex2colour <- function(x, cols.rgb=colours.rgb()) {
-	stopifnot( is.hex.colour(x) )
+#' @rdname mjc-colours
+hex2colour <- function(hex, cols.rgb=colours.rgb()) {
+	stopifnot( is.hex.colour(hex) )
 	# stopifnot( !is.null(rownames(cols.rgb)) )
 
-	rgb <- hex2rgb(x)
+	rgb <- hex2rgb(hex)
 
 	col <- rgb2colour(rgb, cols.rgb)
 	col
 }
 
-#' Convert "rgb" format to "col" format.
-#' 
-#' ie from "c(255,255,255)" to "White"
-#' Note,there are 256^3 possible x's but only length(cols.rgb())
-#' possible outputs, so the closest colour for each x is determined.
-#' This function is the opposite of grDevices:col2rgb, and also is
-#' the opposite of colour2rgb
-#' 
-#' @param x a vector of colours in the "rgb format" format; ie has three
-#'   columns of [0,255] values, one colour per row.
-#' @param cols.rgb the pool of colours with which to map x to in rgb format.
-#'   Leave as default to get the normal "R colours". NB: If \code{cols.rgb}
-#'  doesn't have names, then the index into cols will be
-#'   returned. If \code{cols.rgb} has valid names, then they will be returned.
-#' @return a vector of the name of R colours;\cr
-#' eg c("white", "magenta", "lightblue"), or a vector of indices into the 
-#' \code{cols.rgb} vector, if no valid names were found in the \code{cols.rgb} object.
-#' @author Mark Cowley, 29/3/07
 #' @export
-rgb2colour <- function(x, cols.rgb=colours.rgb()) {
-	stopifnot( is.rgb.colour(x) )
+#' @rdname mjc-colours
+rgb2colour <- function(rgb, cols.rgb=colours.rgb()) {
+	stopifnot( is.rgb.colour(rgb) )
 	# stopifnot( !is.null(rownames(cols.rgb)) )
 
-	res <- rep(0, nrow(x))
-	for(i in 1:nrow(x)) {
-		tmp <- rbind(x[i,], cols.rgb)
+	res <- rep(0, nrow(rgb))
+	for(i in 1:nrow(rgb)) {
+		tmp <- rbind(rgb[i,], cols.rgb)
 		tmpdist <- as.matrix( dist(tmp, method = "euclidean") )[1, 2:nrow(cols.rgb)]
 		res[i] <- which.min(tmpdist)
 	}
@@ -257,22 +260,8 @@ rgb2colour <- function(x, cols.rgb=colours.rgb()) {
 }
 
 
-#' Return the inverse of a colour in hex form so that it can be passed directly
-#' to a plot command.
-#' 
-#' @param col a vector of colours to be reversed
-#' @return a vector of colours
-#' @examples
-#' plot(1:10, col=inverse.colour("purple"))
-#' inverse.colour("red")
-#' # #00FFFF
-#' inverse.colour("green")
-#' # #FF00FF
-#' inverse.colour("blue")
-#' # #FFFF00
-#' 
-#' @author Mark Cowley, 16 May 2006
 #' @export
+#' @rdname mjc-colours
 inverse.colour <- function(col) {
    rgb <- colour2rgb(col)
    rgb <- abs(rgb - rep(255,3))
@@ -282,6 +271,7 @@ inverse.colour <- function(col) {
 
 
 #' Make a gradient of colours.
+#' 
 #' Evenly make \code{steps} colours merging from the colour \code{from} to the colour
 #' \code{to}, optionally via the colour \code{via}
 #' 
@@ -291,11 +281,11 @@ inverse.colour <- function(col) {
 #' @param steps numeric: how many colours do you want?
 #' @param alpha numeric in [0,255] controlling the opacity, from trasparent (0)
 #'  to opaque (255), or in [0.0,1.0].
-#' @author Mark Cowley, 24 August, 2005
 #' @seealso \code{\link[RColorBrewer]{brewer.pal}}
 #' @export
+#' @author Mark Cowley, 2005-08-24
 #' @examples
-#' colour.step(from="green", to="red", via="black", steps=3)
+#' colour.step(from="green", to="red", via="black", steps=11)
 colour.step <- function(from="black", to="red", via=NULL, steps=10, alpha=255) {
    if( !is.null(via) ) {
 		#
@@ -360,12 +350,12 @@ colour.step <- function(from="black", to="red", via=NULL, steps=10, alpha=255) {
 
 
 #' colours.mjc
+#' 
 #' Produce a vector of colours, following a number of colouring schemes.
 #'
 #' @param palette one of a number of named palette's. see usage.
 #' @param N the number of colours
 #' @return a vector of N colours, in hex format
-#' @author Mark Cowley, 2011-10-20
 #' @export
 #' @seealso \code{\link[RColorBrewer]{brewer.pal}}
 #' @examples
