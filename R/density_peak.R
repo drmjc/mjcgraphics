93b4cc95c8cc70132e60d86f48c2f801cb0b5556
Mark Cowley <m.cowley0@gmail.com>
2012-10-21 11:31:36 +0000

    renamed for S3 consistency

--- R/density_peak.R
+++ R/density_peak.R
@@ -3,12 +3,12 @@
 #' @param data a matrix or vector of data
 #' @author Mark Cowley, 12 July 2005
 #' @export
-#' @rdname density.peak
-density.peak <- function(data) {
+#' @rdname density_peak
+density_peak <- function(data) {
 	if( !is.null(ncol(data)) && (ncol(data) > 1) ) {
 		res <- NULL
 		for( i in 1:ncol(data) )
-			res <- rbind(res, density.peak(data[,i]) )
+			res <- rbind(res, density_peak(data[,i]) )
 		if(!is.null(colnames(data)))
 			rownames(res) <- colnames(data)
 		return( as.data.frame( res ) )
@@ -21,5 +21,5 @@ density.peak <- function(data) {
 }
 
 #' @export
-#' @rdname density.peak
-get.density.peaks <- density.peak
+#' @rdname density_peak
+get_density_peaks <- density_peak
